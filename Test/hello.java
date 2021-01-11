b9a834725cc1d357a870d24a6f4a84962f8ab854
sodaop <77297116+sodaop@users.noreply.github.com>
2021-01-12 02:53:19 +0700

    3 hello

--- Test/hello.java
+++ Test/hello.java
@@ -5,7 +5,7 @@ package Test;
 public class hello {
 
 public static void main(String[] args) {
-        System.out.println("hello from saiphirun at CAMT");
+        System.out.println("hello from saiphirun at CAMT, CMU");
     }
 
 }
