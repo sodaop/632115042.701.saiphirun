8433cbc5c6d34cef3ef572228dff2d08f9b72776
sodaop <77297116+sodaop@users.noreply.github.com>
2021-01-12 02:52:04 +0700

    Update hello.java
    2 hello

--- Test/hello.java
+++ Test/hello.java
@@ -5,7 +5,7 @@ package Test;
 public class hello {
 
 public static void main(String[] args) {
-        System.out.println("hello from saiphirun");
+        System.out.println("hello from saiphirun at CAMT");
     }
 
 }
