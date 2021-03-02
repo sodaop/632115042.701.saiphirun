d960fe592d05de19a2a07d89a29622eaca0c1c80
sodaop <77297116+sodaop@users.noreply.github.com>
2021-03-02 18:16:17 +0700

    Demo

---
+++ Week11/Demo.java
@@ -0,0 +1,30 @@
+package Week11;
+
+class A{
+
+}
+class B extends A{
+
+}
+
+class C extends B{
+
+}
+
+public class Demo {
+
+    public static void main(String[] args) {
+        A a= new A();
+        B b = new B();
+        Object c = new C();
+
+        System.out.println(a instanceof A);
+        System.out.println(b instanceof B);
+        System.out.println(b instanceof A);
+        System.out.println(a instanceof B);
+        System.out.println(c instanceof C);
+        System.out.println(c instanceof A);
+        System.out.println(c instanceof B);
+    }
+    
+}
